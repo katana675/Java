cb2de44ae8dd68b9a36a07dc3e89fd7ce0bb435a
katana675 <xzabreak2769cabjkin@gmail.com>
2022-05-12 16:13:05 +0900

    gg

---
+++ bank/Main.java
@@ -0,0 +1,19 @@
+package bank;
+
+public class Main {
+
+	public static void main(String[] args) {
+		calculator t1=new calculator();
+		
+		t1.setbalance(10000);
+		t1.deposit(10000);
+		calculator.deposit(a,b);
+		System.out.println("残高は"+t1.getBalance());
+		t1.payment(5000);
+		System.out.println("残高は"+t1.getBalance());
+		t1.payment(20000);
+//		alret?
+
+	}
+
+}
---
+++ bank/calculator.java
@@ -0,0 +1,22 @@
+package bank;
+
+public class calculator {
+
+	public static void setbalance(int a) {
+	}
+	public static int doposit(int a,int b) {
+		if(price=0) {
+			return -1;
+		}else {
+			return 0;
+		}
+		
+	}
+	public static int payment(int a,int b) {
+		
+	}
+	public static int getBalance() {
+		
+	}
+	
+}
